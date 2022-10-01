525330208396316a0a83bfe805c02fb3dee3f33a
phelipedaniels <79849802+moozespec@users.noreply.github.com>
2022-10-01 03:32:49 +0000

    mudanças organizacionais

---
+++ obrigado.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_POST(['email'])) && !empty )
+
+
+
+$nome = addslashes($_POST(['name']))
+$email = addslashes($_POST(['email']))
+$mensagem = addslashes($_POST(['message']))
+
+
+
+
+
+?>
---
+++ script.js
@@ -0,0 +1,28 @@
+//*código para o gio em tempo real no rodape*//
+  const options = {
+	timeZone: 'America/Sao_Paulo',
+	hour: 'numeric',
+	minute: 'numeric'
+};
+const date = new Intl.DateTimeFormat([], options);
+console.log(date.format(new Date()));
+
+
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+
+//*código para contador de visitas*//
+
+<a href="https://www.webcontadores.com" title="contador de visitas"><img src="https://counter9.stat.ovh/private/webcontadores.php?c=5sdk26uh3r8rcq1sxqjypmg8ftdbwfdk" border="0" title="contador de visitas" alt="contador de visitas"></a>
+
+
+
+
+
+
+
+
+
+
